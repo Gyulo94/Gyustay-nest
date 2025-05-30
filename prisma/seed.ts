065bdbe0128b4bd861a10ad8c15b006c48a29f05
Gyulo94 <qkrcjfrb112@gmail.com>
2025-05-30 18:33:12 +0900

    feat: fnq seed 데이터 및 스키마 추가

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -55,6 +55,19 @@ async function seedCategories() {
   });
 }
 
+async function seedFags() {
+  Array.from({ length: 10 }, (v, i) => i).forEach(async () => {
+    const faqData = {
+      title: faker.lorem.words(),
+      description: faker.lorem.paragraph(),
+    };
+    const response = await prisma.faq.create({
+      data: faqData,
+    });
+    console.log(response);
+  });
+}
+
 async function seedRooms() {
   const totalUsers = await prisma.user.findMany();
   const totalCategories = await prisma.category.findMany();
@@ -135,9 +148,10 @@ function getRandomLongitude() {
 }
 
 async function main() {
-  await seedUsers();
-  await seedCategories();
-  await seedRooms();
+  // await seedUsers();
+  // await seedCategories();
+  // await seedRooms();
+  await seedFags();
 }
 
 main()
